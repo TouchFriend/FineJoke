9d9acbf8d2e2276f5b4fdc2902abed1df0be10d0
cxz <1604101058@qq.com>
2017-07-13 11:09:41 +0800

    将照片保存到对应的App相册中

--- June_nine_BuDeJie(精华)父子控制器/June_nine_BuDeJie/Classes/Essence(精华)/Others/NJSeeBigPictureVC.m
+++ June_nine_BuDeJie(精华)父子控制器/June_nine_BuDeJie/Classes/Essence(精华)/Others/NJSeeBigPictureVC.m
@@ -10,6 +10,7 @@
 #import "NJTopic.h"
 #import "UIImageView+NJDownload.h"
 #import <Photos/Photos.h>
+#import <SVProgressHUD.h>
 @interface NJSeeBigPictureVC () <UIScrollViewDelegate>
 - (IBAction)backBtnClick:(UIButton *)sender;
 - (IBAction)saveBtnClick:(UIButton *)sender;
@@ -18,6 +19,10 @@
 /********* 图片 *********/
 @property(nonatomic,weak)UIImageView * imageView;
 @property (weak, nonatomic) IBOutlet UIButton *saveBtn;
+/********* 获取App对应的相册 *********/
+- (PHAssetCollection *)createdCollection;
+/********* 将图片保存到相机胶卷中 *********/
+- (PHFetchResult<PHAsset *> *)saveImage:(UIImage *)image;
 @end
 
 @implementation NJSeeBigPictureVC
@@ -86,40 +91,115 @@
 {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
+#pragma mark - 将图片保存到相机胶卷中
+- (PHFetchResult<PHAsset *> *)saveImage:(UIImage *)image
+{
+    NSError * error;
+    __block NSString * localIdentifier;
+    //同步执行的
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+        localIdentifier = [PHAssetChangeRequest creationRequestForAssetFromImage:image].placeholderForCreatedAsset.localIdentifier;
+    } error:&error];
+    if(error != nil)
+    {
+        return nil;
+    }
+    //获取新添加到相册的图片
+    return [PHAsset fetchAssetsWithLocalIdentifiers:@[localIdentifier] options:nil];
+}
+#pragma mark - 获取App对应的相册
+- (PHAssetCollection *)createdCollection
+{
+    //获取app的名称
+    NSString * appTitle = [NSBundle mainBundle].infoDictionary[(__bridge NSString *)kCFBundleNameKey];
+    //获取所有的用户自定义的相册
+    PHFetchResult<PHAssetCollection *> * collections = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+    //1.查询app对应的相册是否存在
+    for (PHAssetCollection * collection in collections) {
+        if([collection.localizedTitle isEqualToString:appTitle])
+        {
+            //返回查找到的相册
+            return collection;
+        }
+    }
+    NSError * error;
+    __block NSString *  localIdentifier;
+    //2.创建app对应的相册
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+        localIdentifier = [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle:appTitle].placeholderForCreatedAssetCollection.localIdentifier;
+    } error:&error];
+    if(error != nil)
+    {
+        return nil;
+    }
+    //通过相册的唯一标识获取App对应的相册
+    return [PHAssetCollection fetchAssetCollectionsWithLocalIdentifiers:@[localIdentifier] options:nil].firstObject;
+    
+    
+}
 
+#pragma mark - 点击保存按钮
 - (IBAction)saveBtnClick:(UIButton *)sender
 {
-    /*
-     'This method can only be called from inside of -[PHPhotoLibrary performChanges:completionHandler:] or -[PHPhotoLibrary performChangesAndWait:error:]'
-     */
-    //    NSError * error;
-    //    //同步执行的
-    //    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
-    //        [PHAssetChangeRequest creationRequestForAssetFromImage:self.imageView.image];
-    //    } error:&error];
-    //    if(error == nil)
-    //    {
-    //        NJLog(@"success");
-    //    }
-    //    else
-    //    {
-    //        NJLog(@"error");
-    //    }
-    //异步执行的
-    [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
-        [PHAssetChangeRequest creationRequestForAssetFromImage:self.imageView.image];
-    } completionHandler:^(BOOL success, NSError * _Nullable error) {
-        if(error == nil)
-        {
-            NJLog(@"success");
-        }
-        else
-        {
-            NJLog(@"error");
-        }
-    }];
+    //得到授权状态
+    PHAuthorizationStatus oldStatus = [PHPhotoLibrary authorizationStatus];
+    // 请求\检查访问权限 :
+    // 如果用户还没有做出选择，会自动弹框，用户对弹框做出选择后，才会调用block
+    // 如果之前已经做过选择，会直接执行block
 
-    
+    [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+        dispatch_sync(dispatch_get_main_queue(), ^{//放到主线程中
+            if(status == PHAuthorizationStatusAuthorized)//用户允许当前App访问相册
+            {
+                [self saveImageToPhotos];//保存图片
+            }
+            else if(status == PHAuthorizationStatusDenied)//用户拒绝当前App访问相册
+            {
+                if(oldStatus != PHAuthorizationStatusNotDetermined)//如果就得授权状态不是未决定
+                {
+                    [SVProgressHUD showErrorWithStatus:@"请去设置，允许访问相册"];
+                }
+            }
+            else if(status == PHAuthorizationStatusRestricted)//无法访问相册
+            {
+                [SVProgressHUD showErrorWithStatus:@"因系统原因，无法访问相册"];
+            }
+        });
+        
+    }];
+}
+#pragma mark - 保存图片到Photos
+- (void)saveImageToPhotos
+{
+    //1.将图片保存到相机胶卷
+    PHFetchResult<PHAsset *> * fetchResult = [self saveImage:self.imageView.image];
+    if(fetchResult == nil)
+    {
+        [SVProgressHUD showSuccessWithStatus:@"保存失败"];
+        return;
+    }
+    //2.创建App对应的相册
+    PHAssetCollection * collection = self.createdCollection;
+    if(collection == nil)
+    {
+        [SVProgressHUD showErrorWithStatus:@"保存失败"];
+        return;
+    }
+    //3.将新照片添加到对应的App相册中。
+    NSError * error;
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+        PHAssetCollectionChangeRequest * collectionRequest = [PHAssetCollectionChangeRequest changeRequestForAssetCollection:collection];
+        [collectionRequest insertAssets:fetchResult atIndexes:[NSIndexSet indexSetWithIndex:0]];
+    } error:&error];
+    if(error == nil)
+    {
+        [SVProgressHUD showSuccessWithStatus:@"保存成功"];
+    }
+    else
+    {
+        [SVProgressHUD showErrorWithStatus:@"保存失败"];
+    }
+
 }
 #pragma mark - UIScrollViewDelegate方法
 - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
