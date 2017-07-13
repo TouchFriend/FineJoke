bd0320025f5c6b3868ee844ba477b91ba21010b6
cxz <1604101058@qq.com>
2017-07-13 10:54:55 +0800

    添加全局常量文件

---
+++ June_nine_BuDeJie(精华)父子控制器/June_nine_BuDeJie/Classes/Others/Catogorys/NJGlobalConst.h
@@ -0,0 +1,16 @@
+//
+//  NJGlobalConst.h
+//  June_nine_BuDeJie
+//
+//  Created by TouchWorld on 2017/6/19.
+//  Copyright © 2017年 cxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//UIKIT_EXTERN简单来说，就是将函数修饰为兼容以往C编译方式的、具有extern属性(文件外可见性)、public修饰的方法或变量库外仍可见的属性
+//tabBar的高度
+UIKIT_EXTERN CGFloat const NJTabBarHeight;
+//导航条的最大y
+UIKIT_EXTERN CGFloat const NJNavBarMaxY;
+//标题栏的高度
+UIKIT_EXTERN CGFloat const NJTitleBarHeight;
---
+++ June_nine_BuDeJie(精华)父子控制器/June_nine_BuDeJie/Classes/Others/Catogorys/NJGlobalConst.m
@@ -0,0 +1,17 @@
+//
+//  NJGlobalConst.m
+//  June_nine_BuDeJie
+//
+//  Created by TouchWorld on 2017/6/19.
+//  Copyright © 2017年 cxz. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+/*************** 精华控制器 ********************/
+//tabBar的高度
+CGFloat const NJTabBarHeight = 49;
+//导航条的最大y
+CGFloat const NJNavBarMaxY = 64;
+//标题栏的高度
+CGFloat const NJTitleBarHeight = 40;
+/*************** 精华控制器 ********************/
